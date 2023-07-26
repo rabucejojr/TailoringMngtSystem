6fec9d158268cac5b046aa04238ab23475b44226
rabucejo <rogerhapay@gmail.com>
2023-07-26 14:33:53 +0800

    added card component

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-white shadow-md rounded-lg p-4 border-solid">
+    <h2 class="text-xl font-semibold mb-2">{{ $title }}</h2>
+    <p class="text-gray-600">{{ $content }}</p>
+    <a href="{{ $link }}" class="inline-block mt-2 px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700">See More</a>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,12 @@
     {{-- Navbar Component Here --}}
     @include('components.navbar')
     {{-- Navbar Component Here --}}
-    <div class="container mx-auto p-">
+    <div class="container mx-auto">
         {{-- Content Goes Here --}}
+        {{-- Card Component example called here --}}
+        @component('components.card', ['title' => 'Title 1', 'content' => 'Content1', 'link' => 'Link1'])
+        @endcomponent
+        @component('components.card', ['title' => 'Title 2', 'content' => 'Content2', 'link' => 'Link2'])
+        @endcomponent
     </div>
 @endsection
