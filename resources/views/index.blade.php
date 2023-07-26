dcc8bff25905068c00c397d506f0dcaeb97b5333
rabucejo <rogerhapay@gmail.com>
2023-07-26 14:14:57 +0800

    separated navbar as component

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('partials.app')
+@section('content')
+    {{-- Navbar Component Here --}}
+    @include('components.navbar')
+    {{-- Navbar Component Here --}}
+    <div class="container mx-auto p-">
+        {{-- Content Goes Here --}}
+    </div>
+@endsection
