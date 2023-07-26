12e6265003e0eacb60ae60b852fddfdb26dd9c28
rabucejo <rogerhapay@gmail.com>
2023-07-26 14:02:03 +0800

    added navbar

--- resources/views/partials/app.blade.php
+++ resources/views/partials/app.blade.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>demo</title>
+    <title>TMS</title>
     @vite('resources/css/app.css')
 </head>
 
-<body class="antialiased">
-
-</body>
+@yield('content')
 
 </html>
